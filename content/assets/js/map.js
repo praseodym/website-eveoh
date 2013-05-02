59f38999e0228dd3ff935bfd9112ca371b775231
Sander Ploegsma <sanderploegsma@gmail.com>
2013-05-02 09:43:10 +0200

    Changed the Google Map to API v3

--- content/assets/js/map.js
+++
@@ -1,8 +0,0 @@
-function initialize() {
-    var map = new GMap2(document.getElementById("map"));
-    map.setCenter(new GLatLng(52.0276445,4.3555377), 16);
-    map.setMapType(G_PHYSICAL_MAP);
-
-    var point = new GLatLng(52.0276445,4.3555377);
-    map.addOverlay(new GMarker(point));
-}
